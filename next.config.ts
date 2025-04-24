63d207f2451aa081f70f7e8a4691100d9f89269e
Tanguy VANSNICK <191451@umons.ac.be>
2025-04-24 16:59:56 +0200

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+// Replace <repository-name> with your GitHub repository name
+const repoName = 'screen-wize';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // Add basePath and assetPrefix if deploying to subdirectory
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  // Indicate that images should be unoptimized for `next export`
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
